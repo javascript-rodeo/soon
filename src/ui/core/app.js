0a5831a64052156cd591169e739ccaa6ccebed23
Kevin <kevin.isom@gmail.com>
2020-07-12 09:47:29 +1200

    hash routes

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -1,22 +1,28 @@
 import React, { Component, Fragment } from 'react'
-import { Router } from '@reach/router'
+import { LocationProvider, Router, createHistory } from '@reach/router'
 
+import createHashSource from 'hash-source'
 const About = React.lazy(() => import('../features/about'))
 const Home = React.lazy(() => import('../features/home'))
 const Contact = React.lazy(() => import('../features/contact'))
 const Signup = React.lazy(() => import('../features/signup'))
+
+const source = createHashSource()
+const history = createHistory(source)
 export default class App extends Component {
   render() {
     return (
       <Fragment>
-        <React.Suspense fallback={<div>Loading</div>}>
-          <Router>
-            <Home path="/" />
-            <About path="/about" />
-            <Contact path="/contact" />
-            <Signup path="/register" />
-          </Router>
-        </React.Suspense>
+        <LocationProvider history={history}>
+          <React.Suspense fallback={<div>Loading</div>}>
+            <Router>
+              <Home path="/" />
+              <About path="/about" />
+              <Contact path="/contact" />
+              <Signup path="/register" />
+            </Router>
+          </React.Suspense>
+        </LocationProvider>
       </Fragment>
     )
   }
