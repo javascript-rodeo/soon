d33e86b8e7dab6d4508e1c07008d60c4262e727b
Kevin <kevin.isom@gmail.com>
2020-07-12 09:35:14 +1200

    update new syntax to copy plugin

--- src/webpack/prod.config.js
+++ src/webpack/prod.config.js
@@ -9,12 +9,14 @@ const prodConfig = {
       template: './src/ui/index.html',
       historyApiFallback: true,
     }),
-    new CopyPlugin([
-      {
-        from: path.join(process.cwd(), '/src/public'),
-        to: path.join(process.cwd(), '/dist'),
-      },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(process.cwd(), '/src/public'),
+          to: path.join(process.cwd(), '/dist'),
+        },
+      ],
+    }),
   ],
   output: {
     path: path.join(process.cwd(), '/dist'),
