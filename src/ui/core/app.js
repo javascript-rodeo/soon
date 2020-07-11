1ca7a25ef9f9d667a95183277cc146b649c0d2b1
Kevin <kevin.isom@gmail.com>
2020-07-12 09:33:50 +1200

    Setup initial account

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -4,7 +4,7 @@ import { Router } from '@reach/router'
 const About = React.lazy(() => import('../features/about'))
 const Home = React.lazy(() => import('../features/home'))
 const Contact = React.lazy(() => import('../features/contact'))
-
+const Signup = React.lazy(() => import('../features/signup'))
 export default class App extends Component {
   render() {
     return (
@@ -14,6 +14,7 @@ export default class App extends Component {
             <Home path="/" />
             <About path="/about" />
             <Contact path="/contact" />
+            <Signup path="/register" />
           </Router>
         </React.Suspense>
       </Fragment>
